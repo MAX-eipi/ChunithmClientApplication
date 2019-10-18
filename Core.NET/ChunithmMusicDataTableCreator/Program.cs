d4d148b887fbc6ab0184180df9c2f1a710b0c63e
MAX-eipi <max.eipi.3141@gmail.com>
2019-10-19 04:39:17 +0900

    implement

--- Core.NET/ChunithmMusicDataTableCreator/Program.cs
+++ Core.NET/ChunithmMusicDataTableCreator/Program.cs
@@ -1,9 +1,7 @@
 using ChunithmClientLibrary;
 using ChunithmClientLibrary.ChunithmMusicDatabase.HttpClientConnector;
-using ChunithmClientLibrary.ChunithmNet.API;
-using ChunithmClientLibrary.ChunithmNet.HttpClientConnector;
-using ChunithmClientLibrary.ChunithmNet.Parser;
 using ChunithmClientLibrary.MusicData;
+using ChunithmClientLibrary.Writer;
 using System;
 using System.IO;
 using System.Text;
@@ -12,22 +10,19 @@ namespace ChunithmMusicDataTableCreator
 {
     class Program
     {
+        private enum OutputType
+        {
+            Json,
+            Xlsx,
+            Csv,
+        }
+
         private class Argument
         {
             public string Host { get; private set; }
 
-            public string SegaId { get; private set; } = "";
-            public string Password { get; private set; } = "";
-            public int AimeId { get; private set; } = 0;
-
-            public bool LocalMode { get; private set; } = false;
-            public string SourceDirectoryPath { get; private set; } = "./Sources";
-            public string SourceFileNameForamat { get; private set; } = "music_source_{0}.html";
-
-            public bool OutputXml { get; private set; } = false;
-            public string OutputDirectoryPath { get; private set; } = "./Outputs";
-
-            public int MaxLevel { get; private set; } = 21;
+            public OutputType OutputType { get; private set; } = OutputType.Json;
+            public string OutputPath { get; private set; } = "./table.json";
 
             public Argument(string[] args)
             {
@@ -44,46 +39,14 @@ namespace ChunithmMusicDataTableCreator
                             Host = args[i + 1];
                             i += 2;
                             break;
-                        case "--sega_id":
-                            SegaId = args[i + 1];
+                        case "--output_type":
+                            SetOutputType(args[i + 1]);
                             i += 2;
                             break;
-                        case "--password":
-                            Password = args[i + 1];
+                        case "--output_path":
+                            OutputPath = args[i + 1];
                             i += 2;
                             break;
-                        case "--aime_id":
-                            AimeId = int.Parse(args[i + 1]);
-                            i += 2;
-                            break;
-
-                        case "--local_mode":
-                            LocalMode = true;
-                            i++;
-                            break;
-                        case "--source_path":
-                            SourceDirectoryPath = args[i + 1];
-                            i += 2;
-                            break;
-                        case "--source_file_name_format":
-                            SourceFileNameForamat = args[i + 1];
-                            i += 2;
-                            break;
-
-                        case "--output_xml":
-                            OutputXml = true;
-                            i++;
-                            break;
-                        case "--output_directory_path":
-                            OutputDirectoryPath = args[i + 1];
-                            i += 2;
-                            break;
-
-                        case "--max_level":
-                            MaxLevel = int.Parse(args[i + 1]);
-                            i += 2;
-                            break;
-
                         default:
                             Console.WriteLine("invalid argument[{0}] : {1}", i, args[i]);
                             i++;
@@ -92,14 +55,21 @@ namespace ChunithmMusicDataTableCreator
                 }
             }
 
-            public string GetSourceFilePath(string fileName)
+            private void SetOutputType(string type)
             {
-                return Path.Combine(SourceDirectoryPath, string.Format(SourceFileNameForamat, fileName));
-            }
-
-            public string GetOutputFilePath(string fileName)
-            {
-                return Path.Combine(OutputDirectoryPath, fileName);
+                var compare = type.ToLower();
+                switch (compare)
+                {
+                    case "json":
+                        OutputType = OutputType.Json;
+                        break;
+                    case "xlsx":
+                        OutputType = OutputType.Xlsx;
+                        break;
+                    case "csv":
+                        OutputType = OutputType.Csv;
+                        break;
+                }
             }
         }
 
@@ -107,129 +77,95 @@ namespace ChunithmMusicDataTableCreator
         {
             var argument = new Argument(args);
 
-            var musicDataTable = new MusicDataTable();
-            if (argument.LocalMode)
-            {
-                Console.Write("parsing... ");
-                {
-                    var filePath = argument.GetSourceFilePath("list");
-                    var parser = new MusicGenreParser();
-                    using (var reader = new StreamReader(filePath, Encoding.UTF8))
-                    {
-                        var source = reader.ReadToEnd();
-                        var data = parser.Parse(source);
-                        musicDataTable.Add(data);
-                    }
-                }
-                {
-                    var parser = new MusicLevelParser();
-                    for (var i = 0; i < argument.MaxLevel; i++)
-                    {
-                        var filePath = argument.GetSourceFilePath(i.ToString());
-                        using (var reader = new StreamReader(filePath, Encoding.UTF8))
-                        {
-                            var source = reader.ReadToEnd();
-                            var data = parser.Parse(source);
-                            musicDataTable.Add(data);
-                        }
-                    }
-                }
-                Console.WriteLine("done.");
-            }
-            else
-            {
-                using (var connector = new ChunithmNetHttpClientConnector())
-                {
-                    Console.Write("login... ");
-                    var login = connector.LoginAsync(argument.SegaId, argument.Password).Result;
-                    if (login.Success)
-                    {
-                        Console.WriteLine("success.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("failure.");
-                        WriteLineChunithmNetConnectionError(login);
-                        return;
-                    }
+            var table = GetTable(argument);
+            var writer = GetWriter(argument);
+            writer.Set(table);
 
-                    Console.Write("selecting aime... ");
-                    var aimeSelct = connector.SelectAimeAsync(argument.AimeId).Result;
-                    if (aimeSelct.Success)
-                    {
-                        Console.WriteLine("success.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("failure.");
-                        WriteLineChunithmNetConnectionError(login);
-                        return;
-                    }
-
-                    {
-                        Console.Write("downloading music list... ");
-                        var musicGenre = connector.GetMusicGenreAsync(Genre.All, Difficulty.Master).Result;
-                        if (musicGenre.Success)
-                        {
-                            Console.WriteLine("success.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("failure.");
-                            WriteLineChunithmNetConnectionError(musicGenre);
-                            return;
-                        }
-                        musicDataTable.Add(musicGenre.MusicGenre);
-                    }
-                    {
-                        for (var i = 0; i < argument.MaxLevel; i++)
-                        {
-                            Console.Write("downloading level info ({0}/{1})... ", i + 1, argument.MaxLevel);
-                            var musicLevel = connector.GetMusicLevelAsync(i).Result;
-                            if (musicLevel.Success)
-                            {
-                                Console.WriteLine("success.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("failure.");
-                                WriteLineChunithmNetConnectionError(musicLevel);
-                                return;
-                            }
-                            musicDataTable.Add(musicLevel.MusicLevel);
-                        }
-                    }
-                }
-            }
-
-            if (argument.OutputXml)
+            var directoryPath = Path.GetDirectoryName(argument.OutputPath);
+            if (!Directory.Exists(directoryPath))
             {
-                Console.Write("outputting table file... ");
-                var writer = new MusicDataTableXmlWriter();
-                writer.Set(musicDataTable);
-                writer.Write(argument.GetOutputFilePath("table.xlsx"));
-                Console.WriteLine("done.");
+                Directory.CreateDirectory(directoryPath);
             }
-
-            Console.Write("sending table... ");
-            var databaseConnector = new ChunithmMusicDatabaseHttpClientConnector(argument.Host);
-            var result = databaseConnector.UpdateTableAsync(musicDataTable.MusicDatas).Result;
-            if (result.Success)
-            {
-                Console.WriteLine("success.");
-            }
-            else
-            {
-                Console.WriteLine("failure.");
-            }
+            writer.Write(argument.OutputPath);
 
             Console.WriteLine("completed.");
         }
 
-        private static void WriteLineChunithmNetConnectionError(IChunithmNetApiResponse response)
+        private static IMusicDataTable<IMusicDataTableUnit> GetTable(Argument arg)
         {
-            Console.WriteLine(response.ErrorCode);
-            Console.WriteLine(response.ErrorMessage);
+            using (var connector = new ChunithmMusicDatabaseHttpClientConnector(arg.Host))
+            {
+                Console.WriteLine("get table...");
+                var tableGet = connector.GetTableAsync().Result;
+                if (tableGet.Success)
+                {
+                    Console.WriteLine("success.");
+                }
+                else
+                {
+                    Console.WriteLine("failure.");
+                }
+
+                return tableGet.MusicDataTable;
+            }
+        }
+
+        private static IWriter<IMusicDataTable<IMusicDataTableUnit>> GetWriter(Argument arg)
+        {
+            switch (arg.OutputType)
+            {
+                case OutputType.Json:
+                    return new MusicDataTableJsonWriter();
+                case OutputType.Xlsx:
+                    return new MusicDataTableXmlWriter();
+                case OutputType.Csv:
+                    return new MusicDataTableCsvWriter();
+            }
+            return null;
+        }
+    }
+
+    class MusicDataTableCsvWriter : CsvWriter<IMusicDataTable<IMusicDataTableUnit>>
+    {
+        public override string CreateCsv(IMusicDataTable<IMusicDataTableUnit> data)
+        {
+            var csv = new StringBuilder();
+            AppendHeader(csv);
+            foreach (var unit in data.GetTableUnits())
+            {
+                AppendRow(csv, unit);
+            }
+            return csv.ToString();
+        }
+
+        private void AppendHeader(StringBuilder csv)
+        {
+            Append(csv, "ID");
+            Append(csv, "楽曲名");
+            Append(csv, "ジャンル");
+            Append(csv, "BASIC");
+            Append(csv, "ADVANCED");
+            Append(csv, "EXPERT");
+            Append(csv, "MASTER");
+            Append(csv, "BASIC検証");
+            Append(csv, "ADVANCED検証");
+            Append(csv, "EXPERT検証");
+            Append(csv, "MASTER検証", true);
+        }
+
+        private void AppendRow(StringBuilder csv, IMusicDataTableUnit unit)
+        {
+            Append(csv, unit.Id);
+            Append(csv, unit.Name);
+            Append(csv, Utility.ToGenreText(unit.Genre));
+            Append(csv, unit.GetBaseRating(Difficulty.Basic));
+            Append(csv, unit.GetBaseRating(Difficulty.Advanced));
+            Append(csv, unit.GetBaseRating(Difficulty.Expert));
+            Append(csv, unit.GetBaseRating(Difficulty.Master));
+            Append(csv, unit.VerifiedBaseRating(Difficulty.Basic));
+            Append(csv, unit.VerifiedBaseRating(Difficulty.Advanced));
+            Append(csv, unit.VerifiedBaseRating(Difficulty.Expert));
+            Append(csv, unit.VerifiedBaseRating(Difficulty.Master), true);
         }
     }
 }
