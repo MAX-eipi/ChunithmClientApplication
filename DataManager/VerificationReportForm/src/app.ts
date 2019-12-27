263f1ca514949f77f2224b639d3a447fd83f5788
MAX-eipi <max.eipi.3141@gmail.com>
2019-12-27 19:18:16 +0900

    fix version

--- DataManager/VerificationReportForm/src/app.ts
+++ DataManager/VerificationReportForm/src/app.ts
@@ -1,7 +1,7 @@
 import { VerificationReportForm } from "./VerificationReportForm";
 import { Operator } from "./Operators/Operator";
 
-const VERSION: string = "0.2.4.0";
+const VERSION: string = "0.2.4";
 export function getAppVersion(): string {
     return VERSION;
 }
