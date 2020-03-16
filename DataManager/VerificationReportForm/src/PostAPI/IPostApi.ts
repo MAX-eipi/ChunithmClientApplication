4c561209daaf9ac6c978164480c81f2c6b85966c
MAX-eipi <max.eipi.3141@gmail.com>
2020-03-17 00:58:20 +0900

    delete old project

--- DataManager/VerificationReportForm/src/PostAPI/IPostApi.ts
+++
@@ -1,4 +0,0 @@
-export interface IPostAPI {
-    called(api: string): boolean;
-    invoke(api: string, postData: any): any;
-}
--- DataManager/VerificationReportForm/src/events.ts
+++
@@ -1,9 +0,0 @@
-﻿import { notifyUnverified } from "./operations";
-
-function onNotifyUnverified() {
-    let now = new Date();
-    let hours = now.getHours();
-    if (hours == 9 || hours == 17) {
-        notifyUnverified();
-    }
-}
