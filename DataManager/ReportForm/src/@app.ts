b2780185cfec83d0a816a03ba29ef0f7e6fb2f4c
MAX-eipi <max.eipi.3141@gmail.com>
2020-03-22 20:34:46 +0900

    set version

--- DataManager/ReportForm/src/@app.ts
+++ DataManager/ReportForm/src/@app.ts
@@ -1,6 +1,6 @@
 import { ReportForm } from "./ReportForm/ReportForm";
 
-const VERSION: string = "0.3.0.0";
+const VERSION: string = "0.3.1.2";
 export function getAppVersion(): string {
     return VERSION;
 }
