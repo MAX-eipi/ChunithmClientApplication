e6d2cef2fb94359a84a81d5841dcae6b9bfca321
MAX-eipi <max.eipi.3141@gmail.com>
2020-07-20 01:39:43 +0900

    update test code

--- DataManager/ReportForm/src/@test.ts
+++ DataManager/ReportForm/src/@test.ts
@@ -5,8 +5,6 @@ import { Difficulty } from "./MusicDataTable/Difficulty";
 function testRequestChunirecUpdateMusic() {
     Instance.initialize();
     const chunirecModule = Instance.instance.module.getModule(ChunirecModule);
-    chunirecModule.apiHost = 'http://api.0jocn9xq49ke-chunirec.tk';
-    chunirecModule.apiToken = '814b6c508ce17d9791d932c50688e2f2363db38192f1d5eb5e42be06b868400b';
 
     const params: { musicId: number; difficulty: Difficulty; baseRating: number }[] = [
         {
