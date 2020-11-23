0cfccbad55b869972b3a68a21c8271b4100dc1ef
MAX-eipi <max.eipi.3141@gmail.com>
2020-11-23 12:36:40 +0900

    implement executeForEach

--- DataManager/ReportForm/src/UrlFetch/UrlFetchManager.ts
+++ DataManager/ReportForm/src/UrlFetch/UrlFetchManager.ts
@@ -47,4 +47,36 @@ export class UrlFetchManager {
         }
         return result;
     }
+
+    public static executeForEach(streams: UrlFetch.UrlFetchStream | UrlFetch.UrlFetchStream[]): UrlFetch.Result {
+        if (!Array.isArray(streams)) {
+            streams = [streams];
+        }
+        const length = streams.length;
+        const requests: GoogleAppsScript.URL_Fetch.URLFetchRequest[] = [];
+        for (let i = 0; i < length; i++) {
+            requests.push(streams[i].getRawRequest());
+        }
+
+        const responses: GoogleAppsScript.URL_Fetch.HTTPResponse[] = new Array(length);
+        try {
+            for (let i = 0; i < length; i++) {
+                const req = requests[i];
+                const res = UrlFetchApp.fetch(req.url, req);
+                responses[i] = res;
+            }
+        }
+        catch (e) {
+            return UrlFetchResult.createErrorResult(e);
+        }
+
+        const result: UrlFetchResult = new UrlFetchResult();
+        for (let i = 0; i < length; i++) {
+            streams[i].setRawResponse(responses[i]);
+            if (streams[i].hasError) {
+                result.errors.push(streams[i].error);
+            }
+        }
+        return result;
+    }
 }
